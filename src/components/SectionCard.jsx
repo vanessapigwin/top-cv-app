3f56fd4778a4d7890a9fe7c31b3656821ab5a01a
vanessapigwin <72374043+vanessapigwin@users.noreply.github.com>
2024-12-28 05:13:45 +0800

    add button text toggle

--- src/components/SectionCard.jsx
+++ src/components/SectionCard.jsx
@@ -1,9 +1,16 @@
+import { useState } from 'react';
 import '../styles/sectionCard.css'
 
 export default function SectionCard ({style=null, children}) {
+    const [editing, setEditing] = useState(false)
+
+    function editContent () {
+        setEditing(!editing)
+    }
+
     return (
         <div className='section-card' style={style}>
-            <button>Edit</button>
+            <button onClick={editContent}>{editing ? 'Save' : 'Edit'}</button>
             {children}
         </div>
     );
