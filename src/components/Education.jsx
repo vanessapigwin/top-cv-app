b9b6a90be79d0d0a9c9400bba71a4c7f533aafa0
vanessapigwin <72374043+vanessapigwin@users.noreply.github.com>
2025-01-04 01:09:54 +0800

    toggle educ button display

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,18 +1,41 @@
 import { useState } from "react";
 import { education } from "../data";
+import ButtonTray from "./ButtonTray";
 import SectionCard from "./SectionCard";
+import ModalForm from "./Form";
+
+function EducationEntry({ ed, editing, handleEdit, handleDelete }) {
+  return (
+    <li>
+      <div>
+        <span>
+          {ed.degree}, {ed.school} ({ed.yearGraduated})
+        </span>
+        <ButtonTray visible={editing}>
+          <button>Edit</button>
+          <button>Delete</button>
+        </ButtonTray>
+      </div>
+    </li>
+  );
+}
 
 export default function Education() {
   const [editing, setEditing] = useState(false);
+  const [educList, setEducList] = useState(education);
 
   return (
-    <SectionCard editing={editing}>
-      <h2>Education</h2>
+    <SectionCard editing={editing} onClick={() => setEditing(!editing)}>
+      <div className="title-button-tray">
+        <h2>Education</h2>
+        <ButtonTray visible={editing}>
+          <button>Add</button>
+        </ButtonTray>
+      </div>
+
       <ul>
-        {education.map((ed) => (
-          <li key={ed.key}>
-            {ed.degree}, {ed.school} ({ed.yearGraduated})
-          </li>
+        {educList.map((ed) => (
+          <EducationEntry key={ed.key} ed={ed} editing={editing} />
         ))}
       </ul>
     </SectionCard>
