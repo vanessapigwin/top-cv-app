583a6acc7dceb98801291a23428f23352d8049a9
vanessapigwin <72374043+vanessapigwin@users.noreply.github.com>
2025-01-04 01:36:20 +0800

    add button to show form

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -22,20 +22,64 @@ function EducationEntry({ ed, editing, handleEdit, handleDelete }) {
 
 export default function Education() {
   const [editing, setEditing] = useState(false);
+  const [isAdding, setIsAdding] =useState(false)
   const [educList, setEducList] = useState(education);
+  const [selectedIndex, setSelectedIndex] = useState(null);
+
+  function handleChange() {
+    setEditing(!editing);
+    setIsAdding(false)
+  }
+
+  function showForm () {
+    setIsAdding(true)
+  }
+
+  function handleFormSubmit(e) {
+    e.preventDefault()
+    setEditing(false);
+    setIsAdding(false);
+    setSelectedIndex(null)
+  }
+
+  function handleFormCancel() {
+    setEditing(true);
+    setIsAdding(false);
+    setSelectedIndex(null)
+  }
+
+  function handleAdd(e) {
+    handleFormSubmit(e)
+  }
+
+  function handleEdit() {}
+
+  function handleDelete() {}
 
   return (
-    <SectionCard editing={editing} onClick={() => setEditing(!editing)}>
+    <SectionCard editing={editing} onClick={handleChange}>
       <div className="title-button-tray">
         <h2>Education</h2>
         <ButtonTray visible={editing}>
-          <button>Add</button>
+          <button onClick={showForm}>Add</button>
         </ButtonTray>
       </div>
 
+      { isAdding &&
+        <ModalForm handleFormSubmit={handleFormSubmit} handleFormCancel={handleFormCancel}>
+          <input defaultValue='degree'></input>
+        </ModalForm>     
+      }
+
       <ul>
         {educList.map((ed) => (
-          <EducationEntry key={ed.key} ed={ed} editing={editing} />
+          <EducationEntry
+            key={ed.key}
+            ed={ed}
+            editing={editing}
+            handleEdit={handleEdit}
+            handleDelete={handleDelete}
+          />
         ))}
       </ul>
     </SectionCard>
